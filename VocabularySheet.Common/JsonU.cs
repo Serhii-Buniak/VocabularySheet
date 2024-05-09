5ddcb62b37e524d8bc042a1287963a688f944d9c
Serhii Buniak <90277819+Serhii-Buniak@users.noreply.github.com>
2024-05-09 15:39:43 +0300

    json U

---
+++ VocabularySheet.Common/JsonU.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace VocabularySheet.Common;
+
+public static class JsonU
+{
+    public static readonly JsonSerializerOptions Pretty = new JsonSerializerOptions()
+    {
+        WriteIndented = true
+    };
+
+    public static string Serialize<T>(this JsonSerializerOptions options, T data)
+    {
+        return JsonSerializer.Serialize(data, options);
+    }
+    
+    public static T? Deserialize<T>(this JsonSerializerOptions options, string jsonText)
+    {
+        return JsonSerializer.Deserialize<T>(jsonText, options);
+    }
+}
